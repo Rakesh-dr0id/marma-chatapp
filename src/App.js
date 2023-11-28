ef289024ba0655d782bb0517a4b1ede637bdf2b8
Rakesh prabhu <rakeshprabhu@throughbit.com>
2023-11-28 15:54:57 +0530

    Signup

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import Group from './pages/Group';
 import Settings from './pages/Settings';
 import PageNotFound from './pages/PageNotFound';
+import Home2 from './pages/Home2';
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/home2" element={<Home2 />} />
+
         <Route path="/group" element={<Group />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="*" element={<PageNotFound />} />
