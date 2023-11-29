b4cbd0bcd1da09f54b05cecae9505e82c64bf3da
Rakesh prabhu <rakeshmacho2016@gmail.com>
2023-11-29 12:04:56 +0530

    clean

--- marma-chatapp/src/pages/Group.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Group = () => {
-  return <div>Create Group</div>;
-};
-
-export default Group;
--- marma-chatapp/src/pages/Settings.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Settings = () => {
-  return <div>Update</div>;
-};
-
-export default Settings;
